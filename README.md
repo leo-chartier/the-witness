952bfd5fcbba720bc20fb2481b3820bcf25546ed
leo-chartier <leo.chartier@dartybox.com>
2024-10-28 00:21:40 +0100

    Add data types and README

---
+++ README.md
@@ -0,0 +1,34 @@
+# The Witness Puzzle Solver and Generator
+
+A tool for solving and generating puzzles inspired by the game [The Witness](https://en.wikipedia.org/wiki/The_Witness_(2016_video_game)).
+
+## What is it about?
+
+The project recreates the unique puzzles from *The Witness*, where players navigate mazes following specific rules to reach the exit.
+
+![Cover of The Witness](https://www.metacritic.com/a/img/catalog/provider/6/12/6-1-123612-52.jpg)
+
+As for why I am doing this? I just love the game and really think making both a solver and generator for those difficult mechanics will be a good problem to tackle.
+
+## Usage
+
+This project is still at its beginnings and nothing has been defined yet.
+The goal would be to have a GitHub Page where you would be able to solve a randomly generated puzzle each day.
+
+## Roadmap
+
+- [ ] Solver prototype
+  - [x] Data types
+  - [ ] Unit tests
+  - [ ] Maze solver
+  - [ ] Implement dots solving
+  - [ ] Implement colors solving
+  - [ ] Basic generator (maze, dots, colors)
+- [ ] Specifications
+- [ ] Web engine
+- [ ] Front end
+- [ ] Generator
+
+## Contributing
+
+Contributions are welcome! To get started, either fill an issue or open a pull request.
---
+++ prototype/types.py
@@ -0,0 +1,98 @@
+from enum import Enum
+
+
+
+class Position:
+    __x: int
+    __y: int
+
+    def __init__(self, x: int, y: int) -> None:
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self) -> int:
+        return self.__x
+    
+    @property
+    def y(self) -> int:
+        return self.__y
+    
+    def __add__(self, other):
+        if isinstance(other, Position):
+            return Position(self.__x + other.__x, self.__y + other.__y)
+        return NotImplemented
+    
+    def __sub__(self, other):
+        if isinstance(other, Position):
+            return Position(self.__x - other.__x, self.__y - other.__y)
+        return NotImplemented
+    
+    def __neg__(self) -> "Position":
+        return Position(-self.__x, -self.__y)
+    
+    def __abs__(self) -> "Position":
+        return Position(abs(self.__x), abs(self.__y))
+    
+    def __repr__(self) -> str:
+        return f"({self.__x}, {self.__y})"
+
+
+
+class Color(Enum):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    # TODO: Find other color values
+
+
+
+class IntersectionType(Enum):
+    NORMAL = 0
+    START = 1
+    END = 2
+    DOT = 3
+    START_SYMMETRY = 4
+
+class EdgeType(Enum):
+    NORMAL = 0
+    MISSING = 1
+    BROKEN = 2
+    DOT = 3
+
+class Center:
+    pass
+
+class CenterColor(Center):
+    color: Color
+
+class CenterTetris(Center):
+    shape: str # TODO
+    negative: bool
+    rotatable: bool
+
+class CenterSun(Center):
+    color: Color
+
+class CenterElimination(Center):
+    pass
+
+class CenterTriangle(Center):
+    number: int
+
+
+
+class Puzzle:
+    pass # TODO
+
+
+
+class Move(Enum):
+    UP = Position(0, -1)
+    DOWN = Position(0, 1)
+    LEFT = Position(-1, 0)
+    RIGHT = Position(1, 0)
+
+class Solution:
+    puzzle: Puzzle
+    start: Position
+    moves: list[Move]
