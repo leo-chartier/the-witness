840411bbbb1b9cfe4bee65b831ac6da30c23959c
leo-chartier <leo.chartier@dartybox.com>
2024-10-30 22:48:06 +0100

    Add bruteforcing layout

--- prototype/puzzle.py
+++ prototype/puzzle.py
@@ -1,4 +1,4 @@
-from elements import Center, EdgeType, IntersectionType
+from elements import Center, EdgeType, IntersectionType, Move, Position
 from solution import Solution
 
 
@@ -27,4 +27,33 @@ class Puzzle:
         # TODO: Check for invalid puzzles
     
     def find_solution(self) -> Solution:
+        starts = [
+            Position(x, y)
+            for y, row in enumerate(self.intersections)
+            for x, intersection in enumerate(row)
+            if intersection == IntersectionType.START
+        ]
+        candidates = [Solution(start, []) for start in starts]
+
+        # TODO: Do better than bruteforce
+        while candidates:
+            candidate = candidates.pop(0)
+
+            for move in Move:
+                new_candidate = Solution(candidate.start, candidate.moves + [move])
+
+                if not self.is_path_valid(new_candidate):
+                    continue
+
+                if self.is_solution(new_candidate):
+                    return new_candidate
+                
+                candidates.append(new_candidate)
+
+        raise RuntimeError("No solution found")
+    
+    def is_path_valid(self, path: Solution) -> bool:
+        raise NotImplementedError()
+    
+    def is_solution(self, path: Solution) -> bool:
         raise NotImplementedError()
